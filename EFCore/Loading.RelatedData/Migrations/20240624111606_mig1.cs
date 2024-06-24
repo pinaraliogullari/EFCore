8aa2cd2d9376255ffd1e14b93118332e038963cf
Pınar ALİOĞULLARI <aliogullarpnar@gmail.com>
2024-06-24 15:09:56 +0300

    Loading Related Data-Eager Loading

---
+++ EFCore/Loading.RelatedData/Migrations/20240624111606_mig1.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace Loading.RelatedData.Migrations
+{
+    /// <inheritdoc />
+    public partial class mig1 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Regions",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Regions", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Employees",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Surname = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Salary = table.Column<int>(type: "int", nullable: false),
+                    RegionId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Employees", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Employees_Regions_RegionId",
+                        column: x => x.RegionId,
+                        principalTable: "Regions",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Orders",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    EmployeeId = table.Column<int>(type: "int", nullable: false),
+                    OrderDate = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Orders", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Orders_Employees_EmployeeId",
+                        column: x => x.EmployeeId,
+                        principalTable: "Employees",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Regions",
+                columns: new[] { "Id", "Name" },
+                values: new object[,]
+                {
+                    { 1, "İstanbul" },
+                    { 2, "Tokat" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Employees",
+                columns: new[] { "Id", "Name", "RegionId", "Salary", "Surname" },
+                values: new object[,]
+                {
+                    { 1, "Pinar", 1, 1500, "Aliogullari" },
+                    { 2, "Ufuk", 2, 1500, "Aliogullari" },
+                    { 3, "Serkan", 1, 1500, "Aliogullari" },
+                    { 4, "Emre", 2, 1500, "Kaya" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Orders",
+                columns: new[] { "Id", "EmployeeId", "OrderDate" },
+                values: new object[,]
+                {
+                    { 1, 1, new DateTime(2024, 6, 24, 14, 16, 6, 520, DateTimeKind.Local).AddTicks(7325) },
+                    { 2, 1, new DateTime(2024, 6, 24, 14, 16, 6, 520, DateTimeKind.Local).AddTicks(7340) },
+                    { 3, 2, new DateTime(2024, 6, 24, 14, 16, 6, 520, DateTimeKind.Local).AddTicks(7341) },
+                    { 4, 2, new DateTime(2024, 6, 24, 14, 16, 6, 520, DateTimeKind.Local).AddTicks(7342) },
+                    { 5, 3, new DateTime(2024, 6, 24, 14, 16, 6, 520, DateTimeKind.Local).AddTicks(7343) },
+                    { 6, 3, new DateTime(2024, 6, 24, 14, 16, 6, 520, DateTimeKind.Local).AddTicks(7344) },
+                    { 7, 3, new DateTime(2024, 6, 24, 14, 16, 6, 520, DateTimeKind.Local).AddTicks(7345) },
+                    { 8, 4, new DateTime(2024, 6, 24, 14, 16, 6, 520, DateTimeKind.Local).AddTicks(7346) },
+                    { 9, 4, new DateTime(2024, 6, 24, 14, 16, 6, 520, DateTimeKind.Local).AddTicks(7347) },
+                    { 10, 1, new DateTime(2024, 6, 24, 14, 16, 6, 520, DateTimeKind.Local).AddTicks(7347) },
+                    { 11, 2, new DateTime(2024, 6, 24, 14, 16, 6, 520, DateTimeKind.Local).AddTicks(7348) }
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Employees_RegionId",
+                table: "Employees",
+                column: "RegionId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Orders_EmployeeId",
+                table: "Orders",
+                column: "EmployeeId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Orders");
+
+            migrationBuilder.DropTable(
+                name: "Employees");
+
+            migrationBuilder.DropTable(
+                name: "Regions");
+        }
+    }
+}
---
+++ EFCore/Loading.RelatedData/Program.cs
@@ -0,0 +1,104 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Conventions;
+using System.Reflection;
+
+ApplicationDbContext context = new();
+
+#region Loafing Related Data
+#region Eager Loading
+//eager loading , generate edilen bir sorguya ilişkisel verilerin parça parça eklenmesini sağlayan ve bunu yaparken iradeli/istekli bir şekilde yapmamızı sağlayan bir yöntemdir. 
+#region Include
+// Eager loading operasyonu yapmamızı sağlayan bir fonksiyondur. 
+//Yani üretilen bir sorguya diğer ilişkisel tabloların dahil edilmesini sağlayan bir işleve sahiptir. 
+//Include ile dilediğimiz kadar tabloyu dahil edebiliriz.
+var employees= await context.Employees
+    .Include(e=>e.Orders)
+    .Include(e=>e.Region)
+    .ToListAsync();
+#endregion
+#region ThenInclude
+//Üretilen sorguda include edilen tabloların ilişkili olduğu diğer tabloları da sorguya ekleyebilmek için kullanılan bir fonksiyondur. Eğer ki üretilen sorguya include edilen navigation propertykoleksiyonel bir property ise işte o zaman bu property üzerinden diğer ilişkisel tabloya erişim gösterilememektedir. Böyle bir durumda koleksiyonel propertylerin türlerine erişip, o tür ile ilişkili diğer tabloları da sorguya eklememizi sağlayan fonksiyondur. 
+//var regions = await context.Regions
+//    .Include(r => r.Employees)
+//    .ThenInclude(e => e.Orders)
+//    .ToListAsync();
+#endregion
+#region Filtered Include
+// Sorgulama süreçlerinde Include yaparken sonuçlar üzerinde filtreleme ve sıralama gerçekleştirebilmemizi sağlayan bir özelliktir.
+//Desteklenen fonksiyonlar: Where,OrderBy, OrderByDescending, ThenBy,ThenByDescending,Skip, Take
+//Change Tracker'ın aktif olduğu durumlarda Include ewdilmiş sorgular üzerindeki filtreleme sonuçları beklenmeyen olabilir. Bu durum, daha önce sorgulanmş ve Change Tracker tarafından takip edilmiş veriler arasında filtrenin gereksinimi dışında kalan veriler için söz konusu olacaktır. Bundan dolayı sağlıklı bir filtred include operasyonu için change tracker'ın kullanılmadığı sorguları tercih etmeyi düşünebilirsiniz.
+var regions = await context.Regions
+    .Include(r => r.Employees.Where(e => e.Name.Contains("a")).OrderByDescending(e => e.Surname)).ToListAsync();
+#endregion
+#region Eager Loading İçin Kritik Bir Bilgi
+//EF Core, önceden üretilmiş ve execute edilerek verileri belleğe alınmış olan sorguların verileri, sonraki sorgularda KULLANIR!
+
+//var orders = await context.Orders.ToListAsync();
+
+//var employees = await context.Employees.ToListAsync();
+
+#endregion
+#region AutoInclude - EF Core 6
+//Uygulama seviyesinde bir entitye karşılık yapılan tüm sorgulamalarda "kesinlikle" bir tabloya Include işlemi gerçekleştirlecekse eğer bunu her bir sorgu için tek tek yapmaktansa merkezi bir hale getirmemizi sağlayan özelliktir.Bu işlem onModelCreating metodu altında yapılır.
+
+//var employees = await context.Employees.ToListAsync();
+#endregion
+#region IgnoreAutoIncludes
+//AutoInclude konfigürasyonunu sorgu seviyesinde pasifize edebilmek için kullandığımız fonksiyondur.
+
+//var employees = await context.Employees.IgnoreAutoIncludes().ToListAsync();
+#endregion
+
+
+#endregion
+#endregion
+
+
+
+public class Employee
+{
+    public int Id { get; set; }
+    public string? Name { get; set; }
+    public string? Surname { get; set; }
+    public int Salary { get; set; }
+    public int RegionId { get; set; }
+    public Region Region { get; set; }
+    public List<Order> Orders { get; set; }
+}
+
+public class Order
+{
+    public int Id { get; set; }
+    public int EmployeeId { get; set; }
+    public Employee Employee { get; set; }
+    public DateTime OrderDate { get; set; }
+}
+
+public class Region
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public ICollection<Employee> Employees { get; set; }
+
+
+}
+class ApplicationDbContext:DbContext
+{
+    public DbSet<Employee> Employees { get; set; }
+    public DbSet<Order> Orders { get; set; }
+    public DbSet<Region> Regions { get; set; }
+
+	protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+	{
+		optionsBuilder.UseSqlServer(@"Server=DESKTOP-QE6JDF1\SQLEXPRESS;Database=ApplicationDB;User Id=sa;Password=1q2w3e;TrustServerCertificate=true");
+		base.OnConfiguring(optionsBuilder);
+	}
+
+	protected override void OnModelCreating(ModelBuilder modelBuilder)
+	{
+		modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        modelBuilder.Entity<Employee>().Navigation(e => e.Region).AutoInclude();
+		base.OnModelCreating(modelBuilder);
+	}
+}
